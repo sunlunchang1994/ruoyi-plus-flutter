7240fd540e9d8711fdaaf5f3001ddf32dbf5ea54
sunlunchang <sunlunchang@outlook.com>
2025-09-14 20:27:12 +0800

    update:修复包错误

--- business/system/lib/system/ui/oss/oss_list_page_vd.dart
+++ business/system/lib/system/ui/oss/oss_list_page_vd.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fast/gen/fast_l10n.dart';
-import 'package:fast/fast.dart';
+import 'package:fast/package_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:boxes_flutter/flutter/slc/adapter/page_model.dart';
